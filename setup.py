40b5348261b2261fc94cfdf83b4c81e10da69786
Tobias Roessler <tobiasroessler@gmail.com>
2024-11-12 22:37:53 +0100

    Made lilota-django ready for packaging

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='lilota-django',
-  version='0.0.6',
+  version='0.0.8',
   packages=find_packages(include=['lilota_django']),
   include_package_data=True,
   package_data={
@@ -10,6 +10,6 @@ setup(
   },
   install_requires=[
     "Django>=5.0.6",
-    "lilota>=0.0.5",
-  ],
+    "lilota>=0.0.5"
+  ]
 )
